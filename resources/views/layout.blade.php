7ac6a5d086924d5c8690caca91249d52e9a99a82
Emanuel <emanuelravarotto.s@gmail.com>
2018-10-22 17:33:01 -0300

    add css and boostrap

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,19 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Mi sitio</title>
-	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
-	<style>
-
-		.active{
-			text-decoration: none;
-			color: green ;
-		}
-		.error{
-			color:red;
-			display: inline;
-		}
-
-	</style>
+	<link rel="stylesheet" href="{{ asset('css/app.css')}}">
+	
 </head>
 
 
@@ -27,12 +16,55 @@
 		}
 
 	?>
+
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Blog</a>
+			</div>
+	
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					<li class="{{ activeMenu('/') }}"><a  href="{{route('home')}}">Inicio</a></li>
+					<li class="{{ activeMenu('saludo/*') }}"><a  href="{{ route('saludo') }}">Saludar</a></li>
+					<li class="{{ activeMenu('mensajes/create') }}"><a  href="{{ route('mensajes.create')}}">Contacto</a></li>
+					<li class="{{ activeMenu('mensajes') }}"><a  href="{{ route('mensajes.index')}}">Mensajes</a></li>
+				
+				</ul>
+				<form class="navbar-form navbar-left" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Search">
+					</div>
+					<button type="submit" class="btn btn-default">Submit</button>
+				</form>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Link</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Action</a></li>
+							<li><a href="#">Another action</a></li>
+							<li><a href="#">Something else here</a></li>
+							<li><a href="#">Separated link</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div>
+	</nav>
+
+
 	<header>
 		<nav>
-			<a class="{{ activeMenu('/') }}" href="{{route('home')}}">Inicio</a>
-			<a class="{{ activeMenu('saludo/*') }}" href="{{ route('saludo') }}">Saludar</a>
-			<a class="{{ activeMenu('mensajes/create') }}" href="{{ route('mensajes.create')}}">Contacto</a>
-			<a class="{{ activeMenu('mensajes') }}" href="{{ route('mensajes.index')}}">Mensajes</a>
+			
 		</nav>
 	</header>
 
